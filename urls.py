98ae503c92bd1f73a0b1caebf7fe92f2e5863f84
Jesse Aldridge <JesseAldridge@gmail.com>
2017-02-10 16:15:52 -0800

    write to html table

--- urls.py
+++ urls.py
@@ -1,45 +1,100 @@
-import os, re, sys, logging, shutil
+import os, re, sys, logging, shutil, textwrap
 
-log_path = os.path.expanduser('~/Desktop/log.txt')
-shutil.move(log_path, log_path + '.old')
-logging.basicConfig(filename=log_path, level=logging.DEBUG)
+import jinja2
 
-path = sys.argv[1]
-path = os.path.expanduser(path)
-with open(path) as f:
-    text = f.read()
+class LogLine:
+  def __init__(self, line):
+    self.line = line
 
-regex = '"url": "(.+?)",'
-
-match_lines = []
-for line in text.splitlines():
-    match = re.search(regex, line)
+    self.method = None
+    match = re.search('"method": "([A-Z]+?)"', line)
     if match:
-        match_lines.append([match.group(1), line])
+      self.method = match.group(1)
 
-common_prefix = None
-for url, line in match_lines:
-  logging.debug('url: {}'.format(url))
-  logging.debug('common_prefix: {}'.format(common_prefix))
-  if not common_prefix:
-    common_prefix = url
-    continue
-  for i in range(min(len(url), len(common_prefix))):
-    if url[i] != common_prefix[i]:
+    self.url = None
+    match = re.search('"url": "(.+?)",', line)
+    if match:
+        self.url = match.group(1)
+
+
+def main():
+  log_path = os.path.expanduser('~/Desktop/log.txt')
+  shutil.move(log_path, log_path + '.old')
+  logging.basicConfig(filename=log_path, level=logging.DEBUG)
+
+  path = sys.argv[1]
+  path = os.path.expanduser(path)
+  with open(path) as f:
+      text = f.read()
+
+  log_lines = []
+  for line in text.splitlines():
+    if line:
+      log_lines.append(LogLine(line))
+
+  max_url_len = remove_common_url_prefix(log_lines)
+
+  template = jinja2.Template(textwrap.dedent('''
+    <style>
+    * { font-family: Helvetica }
+
+    table, td, th {
+        border: 1px solid #ddd;
+        text-align: left;
+    }
+
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    th, td {
+        padding: 15px;
+    }
+    </style>
+    <table>
+      <tr><th>method</th><th>url</th><th>full line</th></tr>
+    {% for log_line in log_lines %}
+      <tr>
+        <td>{{log_line.method or ''}} </td>
+        <td>{{log_line.url or ''}} </td>
+        <td>{{log_line.line}}</td>
+      </tr>
+    {% endfor %}
+    </table>
+  '''))
+  print template.render(log_lines=log_lines)
+
+
+def remove_common_url_prefix(log_lines):
+  common_url_prefix = None
+  for log_line in log_lines:
+    url = log_line.url
+    if not url:
+      continue
+    logging.debug('url: {}'.format(url))
+    logging.debug('common_url_prefix: {}'.format(common_url_prefix))
+    if not common_url_prefix:
+      common_url_prefix = url
+      continue
+    for i in range(min(len(url), len(common_url_prefix))):
+      if url[i] != common_url_prefix[i]:
+        break
+    else:
+      i += 1
+    common_url_prefix = common_url_prefix[:i]
+    if not common_url_prefix:
       break
-  else:
-    i += 1
-  common_prefix = common_prefix[:i]
-  if not common_prefix:
-    break
 
-logging.debug('prefix: {}'.format(common_prefix))
+  logging.debug('prefix: {}'.format(common_url_prefix))
 
-max_url_len = 0
-for i, t in enumerate(match_lines):
-  url, line = t
-  match_lines[i][0] = url = url[len(common_prefix):]
-  max_url_len = max(max_url_len, len(url))
+  max_url_len = 0
+  for log_line in log_lines:
+    if not log_line.url:
+      continue
+    log_line.url = log_line.url[len(common_url_prefix):]
+    max_url_len = max(max_url_len, len(log_line.url))
+  return max_url_len
 
-for url, line in match_lines:
-    print '{:<{}} | {}'.format(url, max_url_len, line)
+if __name__ == '__main__':
+  main()
